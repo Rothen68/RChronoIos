860b171dc3d6d3d4eb99a7fb6bd49ff7a7712b70
rcdsm2014 <rcdsm2014@macpf08.local>
2015-04-08 11:51:06 +0200

    fenêtre edition exercice

---
+++ RChronoIOS/EditionExerciceView.h
@@ -0,0 +1,13 @@
+//
+//  EditionExerciceView.h
+//  RChronoIOS
+//
+//  Created by rcdsm2014 on 08/04/2015.
+//  Copyright (c) 2015 rcdsm2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditionExerciceView : UIViewController
+
+@end
---
+++ RChronoIOS/EditionSequenceView.m
@@ -0,0 +1,37 @@
+//
+//  EditionSequenceView.m
+//  RChronoIOS
+//
+//  Created by rcdsm2014 on 08/04/2015.
+//  Copyright (c) 2015 rcdsm2014. All rights reserved.
+//
+
+#import "EditionSequenceView.h"
+
+@interface EditionSequenceView ()
+
+@end
+
+@implementation EditionSequenceView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
