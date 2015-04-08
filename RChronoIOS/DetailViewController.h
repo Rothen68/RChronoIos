84f7f7f9b16141955f7eb4f989be31fee84cc2c5
rcdsm2014 <rcdsm2014@macpf08.local>
2015-04-08 09:23:50 +0200

    Initial Commit

---
+++ RChronoIOS/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  RChronoIOS
+//
+//  Created by rcdsm2014 on 08/04/2015.
+//  Copyright (c) 2015 rcdsm2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ RChronoIOS/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  RChronoIOS
+//
+//  Created by rcdsm2014 on 08/04/2015.
+//  Copyright (c) 2015 rcdsm2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
